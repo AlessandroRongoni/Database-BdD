9bd461529a37a96cee64b4b40019e040cf63d7f8
S1092514 <s1092514@studenti.univpm.it>
2022-01-14 12:10:40 +0100

    aggiunta nuove tabelle

--- tabelle.sql
+++ tabelle.sql
@@ -1,3 +1,5 @@
+/*Entità*/
+
 /*Creazione tabella Cliente Carta Passione Cinema*/
 CREATE TABLE CliCPC (
     CodiceFiscale STRING (16) PRIMARY KEY,
@@ -6,45 +8,13 @@ CREATE TABLE CliCPC (
     DataNascita   DATE        NOT NULL,
     Telefono      STRING (10) NOT NULL,
     Sesso         STRING      NOT NULL,
-    Nazionalità   STRING      NOT NULL,
+    Nazionalita   STRING      NOT NULL,
     NumeroCarta   STRING      NOT NULL,
-    Città         STRING (5)  NOT NULL,
+    Citta         STRING (5)  NOT NULL,
     CAP           STRING (5)  NOT NULL,
     Via           STRING      NOT NULL,
     NumeroCivico  STRING      NOT NULL
 );
-
-/*Operazione inserimento nuovo Cliente Carta Passione Cinema*/		
-INSERT INTO CliCPC (
-                       CodiceFiscale,
-                       Nome,
-                       Cognome,
-                       DataNascita,
-                       Telefono,
-                       Sesso,
-                       Nazionalità,
-                       NumeroCarta,
-                       Città,
-                       CAP,
-                       Via,
-                       NumeroCivico
-                   );
-
-/*Operazione lettura tabella Cliente Carta Passione Cinema*/
-				SELECT 	CodiceFiscale,
-						Nome,
-                        Cognome,
-                        DataNascita,
-                        Telefono,
-                        Sesso,
-                        Nazionalità,
-                        NumeroCarta,
-                        Città,
-                        CAP,
-                        Via,
-                        NumeroCivico
-                     FROM CliCPC;
-
 
 /*Creazione tabella Cliente Online*/					 
 CREATE TABLE CliOnline (
@@ -57,25 +27,80 @@ CREATE TABLE CliOnline (
     Password      STRING      NOT NULL
 );
 
-/*Operazione inserimento nuovo Cliente Online*/
-INSERT INTO CliOnline (
-                          CodiceFiscale,
-                          Nome,
-                          Cognome,
-                          DataNascita,
-                          Telefono,
-                          Email,
-                          Password
-                      );
+/*Creazione tabella Acquisto Bar*/
+CREATE TABLE AcqBar (
+    ID          INTEGER     PRIMARY KEY,
+    Data        DATE        NOT NULL,
+    Ora         TIME        NOT NULL,
+    CostoTotale DECIMAL     NOT NULL,
+    Cliente     STRING (16) NOT NULL
+                            REFERENCES CliCPC (CodiceFiscale) 
+);
+
+/*Creazione tabella Prodotto*/
+CREATE TABLE Prodotto (
+    ID     INTEGER PRIMARY KEY,
+    Nome   STRING  NOT NULL,
+    Prezzo DECIMAL NOT NULL
+);
+
+/*Creazione tabella Genere*/
+CREATE TABLE Genere (
+    Genere STRING PRIMARY KEY
+);
+
+/*Creazione tabella Fim*/
+CREATE TABLE Film (
+    Codice      INTEGER PRIMARY KEY,
+    Titolo      STRING  NOT NULL,
+    Anno        STRING  NOT NULL,
+    Durata      INTEGER NOT NULL,
+    Valutazione DECIMAL
+);
 
 
-/*Operazione lettura tabella Cliente Online*/
 
-				SELECT  	CodiceFiscale,
-                            Nome,
-                            Cognome,
-                            DataNascita,
-                            Telefono,
-                            Email,
-                            Password
-                        FROM CliOnline;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*Relazioni*/
+
+
+
+/*Creazione tabella Appartenenza*/
+CREATE TABLE Appart (
+    Film   STRING REFERENCES Film (Codice),
+    Genere STRING REFERENCES Genere (Genere) 
+);
+
+
+

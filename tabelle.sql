559968888ac38a8a8101212e7ce0a17161a62c95
S1092514 <s1092514@studenti.univpm.it>
2022-01-14 10:18:08 +0100

    Aggiornamento tabella CliCPC
    Prova delle operazioni su CliCPC

--- tabelle.sql
+++ tabelle.sql
@@ -1,20 +1,51 @@
-/*Prova commento*/
-CREATE TABLE Cliente_CPC (
-    codice_fiscale STRING (16) PRIMARY KEY,
-    nome           STRING      NOT NULL,
-    cognome        STRING (20) NOT NULL,
-    sesso          STRING      NOT NULL,
-    nazionalità    STRING      NOT NULL,
-    numero_carta   STRING      NOT NULL,
-    data_nascita   DATE        NOT NULL,
-    telefono       STRING      NOT NULL,
-    CAP            STRING (5)  NOT NULL,
-    città          STRING      NOT NULL,
-    via            STRING      NOT NULL,
-    numero_civico  STRING      NOT NULL
+/*Creazione tabella Clienti Carta Passione Cinema*/
+CREATE TABLE CliCPC (
+    CodiceFiscale STRING (16) PRIMARY KEY,
+    Nome          STRING (20) NOT NULL,
+    Cognome       STRING (20) NOT NULL,
+    DataNascita   DATE        NOT NULL,
+    Telefono      STRING (10) NOT NULL,
+    Sesso         STRING      NOT NULL,
+    Nazionalità   STRING      NOT NULL,
+    NumeroCarta   STRING      NOT NULL,
+    Città         STRING (5)  NOT NULL,
+    CAP           STRING (5)  NOT NULL,
+    Via           STRING      NOT NULL,
+    NumeroCivico  STRING      NOT NULL
 );
 
+/*Operazione inserimento nuovo Cliente Carta Passione Cinema*/		
+INSERT INTO CliCPC (
+                       CodiceFiscale,
+                       Nome,
+                       Cognome,
+                       DataNascita,
+                       Telefono,
+                       Sesso,
+                       Nazionalità,
+                       NumeroCarta,
+                       Città,
+                       CAP,
+                       Via,
+                       NumeroCivico
+                   );
 
+/*Operazione lettura tabella Cliente Carta Passione Cinema*/
+SELECT 					CodiceFiscale,
+						Nome,
+                        Cognome,
+                        DataNascita,
+                        Telefono,
+                        Sesso,
+                        Nazionalità,
+                        NumeroCarta,
+                        Città,
+                        CAP,
+                        Via,
+                        NumeroCivico
+                     FROM CliCPC;
+
+					 
 CREATE TABLE Cliente_Online (
     codice_fiscale STRING (16) PRIMARY KEY,
     Nome           STRING (20) NOT NULL,
